e435edf83a9933bf23e699128414e6252a765382
andrewMacmurray <a.macmurray@icloud.com>
2018-05-09 12:01:52 +0100

    creates anagram assert helper for tests 

--- src/test/java/AnagramGeneratorTest.java
+++ src/test/java/AnagramGeneratorTest.java
@@ -2,23 +2,31 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class AnagramGeneratorTest {
 
+    private void assertAnagram(String input, String... dictionaryWords) {
+        List<String> expectedWords = new ArrayList<>();
+        expectedWords.addAll(Arrays.asList(dictionaryWords));
+        assertEquals(AnagramGenerator.generate(input), expectedWords);
+    }
+
     @Test
     public void testNo() {
-        assertEquals(new ArrayList<String>(Arrays.asList("on", "no")), AnagramGenerator.generate("no"));
+        assertAnagram("no", "on", "no");
     }
 
     @Test
     public void testOn() {
-        assertEquals(new ArrayList<String>(Arrays.asList("on", "no")), AnagramGenerator.generate("on"));
+        assertAnagram("on", "on","no");
     }
 
     @Test
     public void testArt() {
-        assertEquals(new ArrayList<String>(Arrays.asList("rat","tar","art")), AnagramGenerator.generate("art"));
+        assertAnagram("rat","rat","tar", "art");
     }
+
 }
